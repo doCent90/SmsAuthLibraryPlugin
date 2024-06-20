5c3fcd9320959b9b24cbad499d45973e4d145317
Andrey Sucheninov <78255120+doCent90@users.noreply.github.com>
2024-06-20 18:00:52 +0300

    Added localization plugin

---
+++ Packages/com.agava.smsauth/Runtime/Services/WinkLocalization.cs
@@ -0,0 +1,38 @@
+﻿using Lean.Localization;
+using UnityEngine;
+
+namespace Agava.Wink
+{
+    public class WinkLocalization : MonoBehaviour
+    {
+        [SerializeField] private LeanLocalization _leanLocalization;
+
+        public static WinkLocalization Instance;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+
+            Instance = this;
+
+            ChangeLang(Application.systemLanguage);
+        }
+
+        public void ChangeLang(SystemLanguage lang)
+        {
+            switch (lang)
+            {
+                case SystemLanguage.Russian:
+                    SetRuLang();
+                    break;
+                default:
+                    SetEnLang();
+                    break;
+            }
+        }
+
+        private void SetRuLang() => _leanLocalization.CurrentLanguage = "ru";
+
+        private void SetEnLang() => _leanLocalization.CurrentLanguage = "en";
+    }
+}
