d1a021c1feb4c0435663b86abe8ffa8a7d41d075
marriuss <53435883+marriuss@users.noreply.github.com>
2024-06-24 00:43:58 +0400

    Fix

--- Packages/com.agava.smsauth/Runtime/WinkAccessManager.cs
+++ Packages/com.agava.smsauth/Runtime/WinkAccessManager.cs
@@ -132,6 +132,7 @@ namespace Agava.Wink
         private void OnSubscriptionExist()
         {
             HasAccess = true;
+            Authorized = true;
             AuthorizationSuccessfully?.Invoke();
 
             if (PlayerPrefs.HasKey(FirstRegist))
