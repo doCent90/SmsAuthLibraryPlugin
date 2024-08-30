513dc8ec9f7587fb45a51f812ba15e4947037c9c
Andrey Sucheninov <78255120+doCent90@users.noreply.github.com>
2024-08-30 20:53:28 +0300

    Added new reemote to sms delay

--- Packages/com.agava.smsauth/Runtime/UIHandler/TextTimer.cs
+++ Packages/com.agava.smsauth/Runtime/UIHandler/TextTimer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using SmsAuthAPI.Program;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.Scripting;
 
 namespace Agava.Wink
@@ -8,18 +10,28 @@ namespace Agava.Wink
     [Preserve]
     internal class TextTimer : MonoBehaviour
     {
-        [SerializeField] private int _time;
+        private const string RemoteName = "sms-delay-seconds";
+        private const int DefaultTime = 300;
+
         [SerializeField] private TextPlaceholder _timePlaceholder;
 
-        private int _seconds;
+        private int _seconds = 0;
         private Coroutine _coroutine;
 
         public event Action TimerExpired;
 
+        private IEnumerator Start()
+        {
+            if (_seconds <= 0)
+                _seconds = DefaultTime;
+
+            yield return new WaitUntil(() => SmsAuthApi.Initialized);
+            SetRemoteConfig();
+        }
+
         internal void Enable()
         {
             _timePlaceholder.gameObject.SetActive(true);
-            _seconds = _time;
             _coroutine = StartCoroutine(Ticking());
 
             IEnumerator Ticking()
@@ -46,6 +58,33 @@ namespace Agava.Wink
                 StopCoroutine(_coroutine);
                 _coroutine = null;
                 _timePlaceholder.gameObject.SetActive(false);
+            }
+        }
+
+        private async void SetRemoteConfig()
+        {
+            var response = await SmsAuthApi.GetRemoteConfig(RemoteName);
+
+            if (response.statusCode == UnityWebRequest.Result.Success)
+            {
+                if (string.IsNullOrEmpty(response.body))
+                {
+                    _seconds = DefaultTime;
+                    Debug.LogError($"Fail to recieve remote config '{RemoteName}': value is NULL");
+                }
+                else
+                {
+                    bool success = int.TryParse(response.body, out int time);
+
+                    if (success)
+                        _seconds = time;
+                    else
+                        _seconds = DefaultTime;
+                }
+            }
+            else
+            {
+                Debug.LogError($"Fail to recieve remote config '{RemoteName}': BAD REQUEST");
             }
         }
     }
